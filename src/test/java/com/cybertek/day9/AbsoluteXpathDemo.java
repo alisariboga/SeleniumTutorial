aee4dbca2524c82e910dca0aa02415e5a2ed7adc
Ali Sariboga <alican.sariboga.acs@gmail.com>
2023-05-04 16:41:22 +0100

    some fixed code

--- src/test/java/com/cybertek/day9/AbsoluteXpathDemo.java
+++ src/test/java/com/cybertek/day9/AbsoluteXpathDemo.java
@@ -11,10 +11,14 @@ import org.testng.annotations.Test;
 
 public class AbsoluteXpathDemo {
     //We use only inside this class, that's why it's private
-    WebDriver driver = BrowserFactory.getDriver("chrome");
+    private WebDriver driver;
+    String fullNameLocator = "html/body/div/div[2]/div/div/form/div/div[1]/input[@name='full_name']";
+    String emailLocator = "html/body/div/div[2]/div/div/form/div/div[1]/input[@name='email']";
+    String signUpButton = "html/body/div/div[2]/div/div/form/button";
 
     @BeforeMethod
     public void setUp() {
+        driver = BrowserFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.get("http://practice.cybertekschool.com/sign_up");
 
@@ -23,12 +27,12 @@ public class AbsoluteXpathDemo {
     @Test
     public void test1() {
         //Find input box to enter name
-        driver.findElement(By.xpath("html/body/div/div[2]/div/div/form/div/div[1]/input[@name='full_name']")).sendKeys("Java GURU");
+        driver.findElement(By.xpath(fullNameLocator)).sendKeys("Java GURU");
         //find input box to enter email
-        driver.findElement(By.xpath("html/body/div/div[2]/div/div/form/div/div[1]/input[@name='email']")).sendKeys("tomsmith@email.com");
+        driver.findElement(By.xpath(emailLocator)).sendKeys("tomsmith@email.com");
         SeleniumUtils.waitPlease(2);
         //find button to click sign up
-        driver.findElement(By.xpath("html/body/div/div[2]/div/div/form/button")).click();
+        driver.findElement(By.xpath(signUpButton)).click();
         SeleniumUtils.waitPlease(2);
         //expected message
         String expectedMessage = "Internal Server Error";
@@ -41,6 +45,19 @@ public class AbsoluteXpathDemo {
          */
         Assert.assertEquals(actualMessage, expectedMessage);
     }
+
+    @Test
+    public void negativeEmailTest() {
+        String expectingLink = driver.getCurrentUrl();
+        driver.findElement(By.xpath(fullNameLocator)).sendKeys("Tom Smith");
+        driver.findElement(By.xpath(emailLocator)).sendKeys("email");
+        driver.findElement(By.xpath(signUpButton)).click();
+        SeleniumUtils.waitPlease(2);
+        String actualLink = driver.getCurrentUrl();
+        Assert.assertEquals(actualLink, expectingLink);
+
+    }
+
 
     @AfterMethod // it will run after every @test
     public void teardown() {
