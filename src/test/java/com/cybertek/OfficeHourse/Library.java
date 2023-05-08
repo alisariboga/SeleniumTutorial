f715ebf4683b7ea98846e51c6ffd2268abf9ffbb
Ali Sariboga <alican.sariboga.acs@gmail.com>
2023-05-08 09:39:29 +0100

    added some training and changes

--- src/test/java/com/cybertek/OfficeHourse/Library.java
+++ src/test/java/com/cybertek/OfficeHourse/Library.java
@@ -1,4 +1,11 @@
 package com.cybertek.OfficeHourse;
 
 public class Library {
+    public static void sleep(double a) {
+        try {
+            Thread.sleep((long) a * 1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
 }
--- src/test/java/com/cybertek/day1/FirstAutomationScript.java
+++ src/test/java/com/cybertek/day1/FirstAutomationScript.java
@@ -2,11 +2,14 @@ package com.cybertek.day1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class FirstAutomationScript {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Users/alisariboga/Documents/selenium dependencies/drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+//        System.setProperty("webdriver.chrome.driver", "/Users/alisariboga/Documents/selenium dependencies/drivers/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+        WebDriver safariDriver = new SafariDriver();
+        safariDriver.get("https://practice.cybertekschool.com");
+        safariDriver.quit();
     }
 }
